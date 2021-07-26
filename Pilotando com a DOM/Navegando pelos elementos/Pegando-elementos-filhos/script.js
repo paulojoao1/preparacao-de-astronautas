2e88a3e434e6ae3490d223a2b08d7c96616ea46b
Paulo João <pjpaulojoao@Hotmail.com>
2021-07-26 19:08:59 -0300

    Navegar pelos elementos filhos

---
+++ Pilotando com a DOM/Navegando pelos elementos/Pegando-elementos-filhos/script.js
@@ -0,0 +1,11 @@
+// Navegando pelos elementos
+// childNodes children
+const el = document.querySelector('body')
+
+
+// firstChiel firstElementChild
+console.log(el.firstElementChild)
+
+
+// lastChild lastElementChild
+console.log(el.lastElementChild)
