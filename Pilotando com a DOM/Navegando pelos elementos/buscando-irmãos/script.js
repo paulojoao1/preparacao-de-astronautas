0573ae36e6dd08495c15afffafe71ead2da7af0c
Paulo João <pjpaulojoao@Hotmail.com>
2021-07-26 19:09:53 -0300

    Navegar pelos elementos irmãos

---
+++ Pilotando com a DOM/Navegando pelos elementos/buscando-irmãos/script.js
@@ -0,0 +1,8 @@
+// Navegando pelos elementos
+
+const el = document.querySelector('header')
+// nextSibling nextElementSibling
+console.log(el.nextSibling)
+
+// previousSibling previousElementSibling
+console.log(el.previousSibling)
