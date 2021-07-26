6524a772c1e54f8da37501b2cf8e31f303fdbe79
Paulo João <pjpaulojoao@Hotmail.com>
2021-07-26 18:13:06 -0300

    Como buscar e selecionar os elementos na página

---
+++ Pilotando com a DOM/Manipulando conteúdos/Manipulando atributos/script.js
@@ -0,0 +1,13 @@
+// Manipulando elementos
+// Atributos
+
+const header = document.querySelector('header')
+header.setAttribute('id', 'header')
+
+const headerID = document.querySelector('#header')
+
+console.log(headerID.getAttribute('class'))
+
+header.removeAttribute('id')
+
+header.setAttribute('class', 'bg header')
---
+++ Pilotando com a DOM/Manipulando conteúdos/Value/script.js
@@ -0,0 +1,7 @@
+// Manipulando conteúdo
+// value
+const element = document.querySelector('imput')
+
+
+console.log(element.value)
+element.value = "outro valor"
