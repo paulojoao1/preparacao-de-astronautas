67b8ece8ddabeea8bec89889dfa5b1c1edd7c67b
Paulo João <pjpaulojoao@Hotmail.com>
2021-07-26 18:32:21 -0300

    Alterar estilos via javascript e classList

---
+++ Pilotando com a DOM/Manipulando estilos e classes/alterando-estilos-com-style/script.js
@@ -0,0 +1,5 @@
+// Alterando estilos
+const element = document.querySelector('body')
+
+element.style.backgroundColor = "#f9f302"
+console.log(element.style.backgroundColor)
---
+++ Pilotando com a DOM/Manipulando estilos e classes/classList/script.js
@@ -0,0 +1,7 @@
+// Alterando estilos
+// classList
+const element = document.querySelector('body')
+element.classList.add('active', 'green')
+console.log(element.classList)
+// element.classList.remove('artive')
+element.classList.toggle('active')
