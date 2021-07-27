59ff9b52b96d90e47a9dfa88e920ae25e3b5236b
Paulo João <pjpaulojoao@Hotmail.com>
2021-07-27 18:42:33 -0300

    Criar e adicionando elementos no HTML

---
+++ Pilotando com a DOM/Criando e adicionando elementos na página/criando-e-adicionando elementos na página/script.js
@@ -0,0 +1,10 @@
+// Criando e adicionando elementos
+
+// createElement
+const div = document.createElement('div');
+div.innerText = "Olá Devs!"
+
+//append prepend
+const body = document.querySelector('body')
+
+body.prepend(div)
